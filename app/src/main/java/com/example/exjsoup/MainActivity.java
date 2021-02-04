3a5962002a3329fe122075a1dfda6e65acbe42ef
NaHyeonJeong <pigismile@gmail.com>
2021-02-04 18:41:57 +0900

    검색어 입력 기능(EditText), 제품 링크 가져오기(href)

--- app/src/main/java/com/example/exjsoup/MainActivity.java
+++ app/src/main/java/com/example/exjsoup/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,13 +17,17 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 
+/**
+ * 소녀나라 홈페이지에서 검색한 내용을 가져오는 App
+ * 제품명과 제품의 링크를 가져옴
+ */
 public class MainActivity extends AppCompatActivity {
-    /**
-     * 소녀나라 사이트에서 150이라는 키워드가 붙은 제품명을 모두 가져옴
-     * 사이트에서 미리 150을 검색한 페이지 URL 을 가져옴
-     */
-    private String url = "https://sonyunara.com/shop/search.php?searchOrder=&keyword=150";
+
+    // TODO 링크 찍고 들어갈 수 있는 방법 찾기
+
+    private String url = "https://sonyunara.com/shop/search.php?searchOrder=&keyword="; // 검색 키워드는 빈칸으로 보냄
     private String htmlContentInStringFormat;
+    EditText keyword;
     TextView tv;
     Button btn;
 
@@ -31,14 +36,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        keyword = (EditText)findViewById(R.id.searchKey);
         tv = (TextView)findViewById(R.id.data);
         btn = (Button)findViewById(R.id.button);
+
 
         tv.setMovementMethod(new ScrollingMovementMethod());
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                JsoupAsyncTask jsoupAsyncTask = new JsoupAsyncTask();
+                MainActivity.JsoupAsyncTask jsoupAsyncTask = new MainActivity.JsoupAsyncTask();
                 jsoupAsyncTask.execute();
             }
         });
@@ -53,14 +60,21 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected Void doInBackground(Void... voids) {
             try {
+                // EditText에 입력한 글자를 가져와서 url에 추가해줌
+                String findkey = keyword.getText().toString();
+                url += findkey;
                 // 웹에서 내용을 가져옴
                 Document doc = Jsoup.connect(url).get();
                 // 내용 중 원하는 부분을 가져옴
-                Elements links = doc.select(".subject"); // class 명이 subject 인 항목을 가져옴
-                for (Element link : links) { // for 문을 활용해서 모든 항목을 가져옴
+                // class 명이 subject 인 element 의 아래에 있는 a element 의 href 속성 값 가져옴 | 제품 명 가져옴
+                Elements subject = doc.select(".subject a[href]");
+                // attr() : 기본적으로 String 형 | 제품의 링크를 가져옴
+                String subjectLink = subject.attr("abs:href");
+                for (Element link : subject) { // for 문을 활용해서 모든 항목을 가져옴
                     // 원하는 부분은 element 형태이므로 String 형태로 바꿔줌
-                    System.out.println("data : " + link.text());
-                    htmlContentInStringFormat += link.text().trim() + "\n";
+                    System.out.println("data : " + link.text()); // terminal 에 찍음
+                    // 앱 화면에 찍을 내용을 변수에 저장
+                    htmlContentInStringFormat += link.text().trim() + "\n" + subjectLink + "\n-------------------------------------\n";
                 }
             } catch (IOException e) { // Jsoup 의 connect 부분에서 오류 발생 가능성이 있으므로 예외 처리
                 e.printStackTrace();
@@ -69,8 +83,10 @@ public class MainActivity extends AppCompatActivity {
         }
         @Override
         protected void onPostExecute(Void result) {
+            tv.setVisibility(View.VISIBLE); // TextView를 gone으로 해두었기 때문에 visible로 바꿔줘야함
             tv.setText(htmlContentInStringFormat);
         }
 
     }
+
 }
