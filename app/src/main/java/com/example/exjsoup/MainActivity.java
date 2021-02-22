bcb2ce188c35615cac8e36bd3e5ddafbd4a3d969
NaHyeonJeong <pigismile@gmail.com>
2021-02-22 20:25:08 +0900

    제품 링크 추가

--- app/src/main/java/com/example/exjsoup/MainActivity.java
+++ app/src/main/java/com/example/exjsoup/MainActivity.java
@@ -13,9 +13,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.safety.Cleaner;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * 소녀나라 홈페이지에서 검색한 내용을 가져오는 App
@@ -23,12 +25,10 @@ import java.io.IOException;
  */
 public class MainActivity extends AppCompatActivity {
 
-    // TODO 링크를 찍고 이동할 수 있도록 하기
-
-    private String url = "https://sonyunara.com/shop/search.php?searchOrder=&keyword="; // 검색 키워드는 빈칸으로 보냄
-    private String htmlContentInStringFormat;
+    private String htmlContentInStringFormat = "";
+    private String htmlContentInStringFormat2;
     EditText keyword;
-    TextView tv;
+    TextView productTv, linkTv;
     Button btn;
 
     @Override
@@ -37,16 +37,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         keyword = (EditText)findViewById(R.id.searchKey);
-        tv = (TextView)findViewById(R.id.data);
+        productTv = (TextView)findViewById(R.id.productData);
+        linkTv = (TextView)findViewById(R.id.linkData);
         btn = (Button)findViewById(R.id.button);
 
-
-        tv.setMovementMethod(new ScrollingMovementMethod());
+        productTv.setMovementMethod(new ScrollingMovementMethod());
+        linkTv.setMovementMethod(new ScrollingMovementMethod());
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 MainActivity.JsoupAsyncTask jsoupAsyncTask = new MainActivity.JsoupAsyncTask();
                 jsoupAsyncTask.execute();
+                productTv.setText("");
+                linkTv.setText("");
             }
         });
     }
@@ -59,22 +62,29 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected Void doInBackground(Void... voids) {
+            String url = "https://sonyunara.com/shop/search.php?searchOrder=&keyword="; // 검색 키워드는 빈칸으로 보냄
             try {
-                // EditText에 입력한 글자를 가져와서 url에 추가해줌
-                String findkey = keyword.getText().toString();
-                url += findkey;
+                // EditText 에 입력한 글자를 가져와서 url 에 추가해줌
+                String findKey = keyword.getText().toString();
+
+                System.out.println("findKey = " + findKey);
+                url += findKey;
+                System.out.println("url = " + url);
                 // 웹에서 내용을 가져옴
                 Document doc = Jsoup.connect(url).get();
                 // 내용 중 원하는 부분을 가져옴
                 // class 명이 subject 인 element 의 아래에 있는 a element 의 href 속성 값 가져옴 | 제품 명 가져옴
-                Elements subject = doc.select(".subject a[href]");
+                Elements subject = doc.select(".subject a");
                 // attr() : 기본적으로 String 형 | 제품의 링크를 가져옴
+                // TODO : 제품 링크가 첫 제품 링크로 도배된다... 각 제품별 링크로 바꿔야 함
                 String subjectLink = subject.attr("abs:href");
                 for (Element link : subject) { // for 문을 활용해서 모든 항목을 가져옴
                     // 원하는 부분은 element 형태이므로 String 형태로 바꿔줌
-                    System.out.println("data : " + link.text()); // terminal 에 찍음
+                    System.out.println("data(p) : " + link.text()); // terminal 에 찍음
+                    System.out.println("data(l) : " + subjectLink); // terminal 에 찍음
                     // 앱 화면에 찍을 내용을 변수에 저장
-                    htmlContentInStringFormat += link.text().trim() + "\n" + subjectLink + "\n-------------------------------------\n";
+                    htmlContentInStringFormat += link.text().trim() + "\n"; // trim() : 문자열 앞/뒤 공백 삭제
+                    htmlContentInStringFormat2 += subjectLink + "\n";
                 }
             } catch (IOException e) { // Jsoup 의 connect 부분에서 오류 발생 가능성이 있으므로 예외 처리
                 e.printStackTrace();
@@ -83,8 +93,10 @@ public class MainActivity extends AppCompatActivity {
         }
         @Override
         protected void onPostExecute(Void result) {
-            tv.setVisibility(View.VISIBLE); // TextView를 gone으로 해두었기 때문에 visible로 바꿔줘야함
-            tv.setText(htmlContentInStringFormat);
+            productTv.setVisibility(View.VISIBLE); // TextView를 gone으로 해두었기 때문에 visible로 바꿔줘야함
+            linkTv.setVisibility(View.VISIBLE);
+            productTv.setText(htmlContentInStringFormat);
+            linkTv.setText(htmlContentInStringFormat2);
         }
 
     }
